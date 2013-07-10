a2a6f7194bc50f41a7d923deceeeb8bd49c62a30
Riley Moses <victi.resurgo@gmail.com>
2013-07-10 14:11:42 -0600

    Allow both PUT and POST to SimpleHTTPServerWithUpload

--- SocketUpload.java
+++ SocketUpload.java
@@ -5,30 +5,50 @@ import java.net.InetSocketAddress;
 
 public class SocketUpload
 {
+	public static final String PUT = "PUT";
+	public static final String POST = "POST";
+	public static final String POST_BOUNDARY = "--------------------fbaf27c439a7d6220a7c6ad2854084fb";
+
 	public static void main(String[] args)
 	{
-		String filename = "/home/riley/Pictures/Wallpaper/sD6tTRAh.jpg";
-		if (args.length != 0)
+		if (args.length != 4)
 		{
-			filename = args[0];
+			System.out.println("Error: Must specify method, hostname, port, and filename");
+			System.exit(1);
 		}
-		System.out.println("Filename is " + filename);
-		uploadFile(filename, "192.168.56.131", 8000);
+		if (!(args[0].equalsIgnoreCase(PUT) || args[0].equalsIgnoreCase(POST)))
+		{
+			System.out.println("Error: Method must be PUT or POST");
+			System.exit(1);
+		}
+
+		String method = args[0].toUpperCase();
+		String hostname = args[1];
+		int port = Integer.parseInt(args[2]);
+		String filename = args[3];
+		System.out.println(method + " " + filename + " to " + hostname + ":" + port);
+		// System.out.println("Method is " + method);
+		// System.out.println("Filename is " + filename);
+
+		uploadFile(method, filename, hostname, port);
 	}
 
-	private static void uploadFile(String fileName, String hostname, int port)
+	private static void uploadFile(String method, String fileName, String hostname, int port)
 	{
 		SocketChannel socket = null;
-		RandomAccessFile zip = null;
+		RandomAccessFile f = null;
+		String header = null;
+		ByteBuffer request = null;
 
 		try
 		{
-			zip = new RandomAccessFile(fileName, "r");
-			byte[] bytes = new byte[(int)zip.length()];
-			zip.readFully(bytes, 0, bytes.length);
-			zip.close();
+			// Get bytes of file
+			f = new RandomAccessFile(fileName, "r");
+			byte[] contentBytes = new byte[(int)f.length()];
+			f.readFully(contentBytes, 0, contentBytes.length);
+			f.close();
 
-			// Create PUT request
+			// Determine the filename
 			int spot = fileName.lastIndexOf('/');
 			if (spot == -1)
 			{
@@ -36,28 +56,59 @@ public class SocketUpload
 
 			}
 			String fn = fileName.substring(spot);
-			System.out.println("file: " + fn);
 
-			String header = "PUT " + fn + " HTTP/1.1\r\n";
-			header += "Content-Length: " + bytes.length + "\r\n\r\n";
-
-			System.out.println("Header:");
-			System.out.println(header);
-
+			// Create the SocketChannel
 			socket = SocketChannel.open();
-			//socket.configureBlocking(false);
 			socket.connect(new InetSocketAddress(hostname, port));
 
-			byte[] headerBytes = header.getBytes("UTF-8");
-			ByteBuffer buf = ByteBuffer.allocate(headerBytes.length + bytes.length);
-			buf.clear();
-			buf.put(headerBytes);
-			buf.put(bytes);
-			buf.flip();
-
-			while(buf.hasRemaining())
+			// Build the request
+			if (method.equals(PUT))
 			{
-				socket.write(buf);
+				header = "PUT " 
+					+ fn 
+					+ " HTTP/1.1\r\nContent-Length: " 
+					+ contentBytes.length 
+					+ "\r\n\r\n";
+				byte[] headerBytes = header.getBytes("UTF-8");
+				
+				request = ByteBuffer.allocate(headerBytes.length + contentBytes.length);
+				request.clear();
+				request.put(headerBytes);
+				request.put(contentBytes);
+			}
+			else if (method.equals(POST))
+			{
+				// POST data must be enclosed by the POST_BOUNDARY
+				String contentStart = "--"
+					+ POST_BOUNDARY
+					+ "\r\n"
+					+ "Content-Disposition: form-data; name=\"file\"; filename=\"" + fn.substring(1) + "\"\r\n"
+					+ "Content-Type: application/octet-stream\r\n\r\n";
+				String contentEnd = "\r\n--" + POST_BOUNDARY + "--";
+				byte[] openingBytes = contentStart.getBytes("UTF-8");
+				byte[] closingBytes = contentEnd.getBytes("UTF-8");
+
+				// Content-Length must include the POST_BOUNDARY wrapper
+				header = "POST / HTTP/1.1\r\nContent-Length: " 
+					+ (openingBytes.length + contentBytes.length + closingBytes.length)
+					+ "\r\nContent-Type: multipart/form-data; boundary=" 
+					+ POST_BOUNDARY 
+					+ "\r\n\r\n";
+				byte[] headerBytes = header.getBytes("UTF-8");
+
+				request = ByteBuffer.allocate(headerBytes.length + openingBytes.length + contentBytes.length + closingBytes.length);
+				request.clear();
+				request.put(headerBytes);
+				request.put(openingBytes);
+				request.put(contentBytes);
+				request.put(closingBytes);
+			}
+
+			// Send the request
+			request.flip();
+			while(request.hasRemaining())
+			{
+				socket.write(request);
 			}
 		}
 		catch (Exception e)
@@ -70,7 +121,7 @@ public class SocketUpload
 			try
 			{
 				socket.close();
-				zip.close();
+				f.close();
 			}
 			catch (Exception e)
 			{

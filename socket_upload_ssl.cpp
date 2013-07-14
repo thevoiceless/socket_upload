314be0b94486a71e10a9808297873e54f69fcfe1
Riley Moses <victi.resurgo@gmail.com>
2013-07-14 17:33:11 -0600

    Code cleanup and comments

--- socket_upload_ssl.cpp
+++ socket_upload_ssl.cpp
@@ -1,3 +1,5 @@
+// Largely based off of https://github.com/polarssl/polarssl/blob/master/programs/ssl/ssl_client1.c
+
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -27,10 +29,21 @@ ctr_drbg_context ctr_drbg;
 
 int ret, server_fd;
 unsigned char buf[1536];
-const char* pers = "ssl_client1";
+const char* identity = "socket upload script";
 
+// SSL-related cleanup
+void cleanUp()
+{
+	x509_free(&cacert);
+	net_close(server_fd);
+	ssl_free(&ssl);
+	memset(&ssl, 0, sizeof(ssl));
+}
+
+// Clean up and exit with the given return code
 int finish(int ret)
 {
+	// PolarSSL has some fancy error logic
 #ifdef POLARSSL_ERROR_C
 	if (ret != 0)
 	{
@@ -40,32 +53,34 @@ int finish(int ret)
 	}
 #endif
 
-	x509_free(&cacert);
-	net_close(server_fd);
-	ssl_free(&ssl);
-	memset(&ssl, 0, sizeof(ssl));
-
+	cleanUp();
 	exit(ret);
 }
 
+// Read all of the bytes from the given file and return them as a vector of characters
 vector<char> readAllBytes(const char* filename)
 {
+	// Open the file in binary mode, starting at the end
 	ifstream infile(filename, ios::binary | ios::ate);
 	if (infile.fail())
 	{
 		cout << "Failed to open file " << filename << endl;
+		cleanUp();
 		exit(1);
 	}
 
+	// The current position is at the end of the file, so tellg() will return its size
 	ifstream::pos_type size = infile.tellg();
 	vector<char> result(size);
 
+	// Return to the beginning of the file and read its contents
 	infile.seekg(0, ios::beg);
 	infile.read(&result[0], size);
 
 	return result;
 }
 
+// Return the current GMT date and time for use in the HTTP header
 string currentDateTime()
 {
 	time_t rawtime;
@@ -79,7 +94,8 @@ string currentDateTime()
 	return buf;
 }
 
-
+// Encode the given bytes in base-64 and return them as a string
+// http://www.adp-gmbh.ch/cpp/common/base64.html
 string base64_encode(const unsigned char* bytes_to_encode, unsigned int in_len)
 {
 	string ret;
@@ -149,7 +165,7 @@ int main(int argc, char* argv[])
 		port = atoi(argv[2]);
 		file = string(argv[3]);
 	}
-	cout << "Hello world" << endl;
+	cout << "PUT " << file << " at " << hostname << ":" << port << endl;
 
 	// Initialize the RNG and the session data
 	memset(&ssl, 0, sizeof(ssl_context));
@@ -157,11 +173,11 @@ int main(int argc, char* argv[])
 
 	cout << "  . Seeding the random number generator...";
 	entropy_init(&entropy);
-	if ((ret = ctr_drbg_init(&ctr_drbg, 
+	if ((ret = ctr_drbg_init(&ctr_drbg,
 		entropy_func, 
 		&entropy,
-		(const unsigned char*) pers,
-		strlen(pers))) != 0)
+		(const unsigned char*) identity,
+		strlen(identity))) != 0)
 	{
 		cout << " failed" << endl << "  ! ctr_drbg_init returned" << ret << endl;
 		finish(ret);
@@ -173,12 +189,14 @@ int main(int argc, char* argv[])
 
 	// Initialize certificates
 	cout << "  . Loading the CA root certificate...";
+
 #if defined(POLARSSL_CERTS_C)
 	ret = x509parse_crt(&cacert, (const unsigned char*) test_ca_crt, strlen(test_ca_crt));
 #else
 	ret = 1;
 	cout << "POLARSSL_CERTS_C not defined.";
 #endif
+
 	if (ret < 0)
 	{
 		cout << " failed" << endl << "  ! x509parse_crt returned -0x" << hex << ret << endl << endl;
@@ -218,7 +236,6 @@ int main(int argc, char* argv[])
 	ssl_set_authmode(&ssl, SSL_VERIFY_OPTIONAL);
 	ssl_set_ca_chain(&ssl, &cacert, NULL, "s3.amazonaws.com");
 	ssl_set_rng(&ssl, ctr_drbg_random, &ctr_drbg);
-	// ssl_set_dbg(&ssl, my_debug, stdout);
 	ssl_set_bio(&ssl, net_recv, &server_fd, net_send, &server_fd);
 
 	// Handshake
@@ -254,14 +271,18 @@ int main(int argc, char* argv[])
 		{
 			cout << "  ! self-signed or not signed by a trusted CA" << endl;
 		}
-		cout << endl;
 	}
 	else
 	{
 		cout << " OK" << endl;
 	}
 
-	// Build request
+	/*
+	 * Build the PUT request
+	 * http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPUT.html
+	 * http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html#ConstructingTheAuthenticationHeader
+	 */
+	// Extract only the file name
 	int spot = file.rfind("/");
 	if (spot == string::npos)
 	{
@@ -271,67 +292,64 @@ int main(int argc, char* argv[])
 			spot = 0;
 		}
 	}
+	// Read all bytes from the file
 	vector<char> fileBytes = readAllBytes(file.c_str());
 	string fileContents(fileBytes.begin(), fileBytes.end());
 
-	// http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html#ConstructingTheAuthenticationHeader
+	// AWS key must be set in an environment variable
 	char* awskey_env = getenv("AWS_KEY");
 	if (!awskey_env)
 	{
 		cout << "No AWS key set in environment vars" << endl;
+		cleanUp();
 		exit(1);
 	}
 	string awskey(awskey_env);
 
+	// Get current time
 	string now = currentDateTime();
 
+	// Construct Amazon's StringToSign element
 	ostringstream stringToSign;
-	stringToSign << "PUT\n";
-	// unsigned char md5output[16];
-	// if (md5_file(file.c_str(), md5output) != 0)
-	// {
-	// 	cout << "Error calculating MD5" << endl;
-	// 	exit(1);
-	// }
-	// else
-	// {
-	// 	cout << "md5 is " << md5output << endl;
-	// }
-	stringToSign << /*md5output << */ "\n";
-	stringToSign << "application/octet-stream\n";
-	stringToSign << now << "\n";
+	stringToSign << "PUT\n";                        // HTTP verb
+	stringToSign << "\n";                           // MD5
+	stringToSign << "application/octet-stream\n";   // Content type
+	stringToSign << now << "\n";                    // Date
 
-	ostringstream cResource;
-	cResource << "/" + hostname.substr(0, hostname.find_first_of('.'));
-	cResource << file.substr(spot, string::npos);
+	// Construct Amazon's CanonicalizedResource element
+	ostringstream canonicalizedResource;
+	canonicalizedResource << "/" + hostname.substr(0, hostname.find_first_of('.')); // Bucket name preceded by a '/'
+	canonicalizedResource << file.substr(spot, string::npos);                       // Path
 
-	stringToSign << cResource.str();
+	// Add CanonicalizedResource to StringToSign
+	// NOTE: Does not include CanonicalizedAmzHeaders element
+	stringToSign << canonicalizedResource.str();
 
-	// cout << "s2s:\n" << stringToSign.str() << endl << endl;
+	// Calculate the HMAC-SHA1 of the access key with the StringToSign element
+	// NOTE: Does not guarantee that StringToSign is in UTF-8 encoding
+	const unsigned char* u_awskey = reinterpret_cast<const unsigned char*>(awskey.c_str());
+	const unsigned char* u_stringToSign = reinterpret_cast<const unsigned char*>(stringToSign.str().c_str());
+	unsigned char sha1hmacOutput[20];
+	sha1_hmac(u_awskey, awskey.length(), u_stringToSign, stringToSign.str().length(), sha1hmacOutput);
 
-	const unsigned char* uawskey = reinterpret_cast<const unsigned char*>(awskey.c_str());
-	const unsigned char* us2s = reinterpret_cast<const unsigned char*>(stringToSign.str().c_str());
-	// cout << "uawskey:\n" << uawskey << endl << endl;
-	// cout << "us2s:\n" << us2s << endl << endl;
-	unsigned char sha1hmacoutput[20];
-	sha1_hmac(uawskey, awskey.length(), us2s, stringToSign.str().length(), sha1hmacoutput);
+	// Create the HTTP header
+	ostringstream header;
+	header << "PUT " << file.substr(spot, string::npos) << " HTTP/1.1\r\n";
+	header << "Content-Type: application/octet-stream\r\n";
+	header << "Content-Length: " << fileContents.size() << "\r\n";
+	header << "Host: " << hostname << "\r\n";
+	header << "Date: " << now << "\r\n\r\n";
+	// HMAC-SHA1 output must be encoded in base-64
+	header << "Authorization: AWS " << awskey << ":" << base64_encode(sha1hmacOutput, 20) << "\r\n\r\n";
 
-	ostringstream request;
-	request << "PUT " << file.substr(spot, string::npos) << " HTTP/1.1\r\n";
-	request << "Content-Type: application/octet-stream\r\n";
-	request << "Content-Length: " << fileContents.size() << "\r\n";
-	request << "Host: " << hostname << "\r\n";
-	request << "Date: " << now << "\r\n\r\n";
-	request << "Authorization: AWS " << awskey << ":" << base64_encode(sha1hmacoutput, 20) << "\r\n\r\n";
+	string headerString = header.str();
+	// cout << headerString << endl;
+	// cout << fileContents << endl;
 
-
-	// request << fileContents;
-	string requestString = request.str();
-	cout << requestString << endl;
-
-	// Send request
-	cout << "  Sending request..." << endl;
-	int len = sprintf((char*)buf, requestString.c_str());
+	// Send the request
+	cout << "  . Sending request..." << endl;
+	// Send header first
+	int len = sprintf((char*) buf, headerString.c_str());
 	while ((ret = ssl_write(&ssl, buf, len)) <= 0)
 	{
 		if (ret != POLARSSL_ERR_NET_WANT_READ && ret != POLARSSL_ERR_NET_WANT_WRITE)
@@ -340,11 +358,10 @@ int main(int argc, char* argv[])
 			finish(ret);
 		}
 	}
-
-	cout << "    " << ret << " header bytes sent" << endl;
+	cout << "  + " << ret << " header bytes sent" << endl;
 	cout.flush();
-
-	// len = sprintf((char*)buf, fileContents.c_str());
+	// Then send the contents
+	// For some reason this was necessary to ensure that the entirety of fileContents was sent
 	const unsigned char* contents = reinterpret_cast<const unsigned char*>(fileContents.c_str());
 	while ((ret = ssl_write(&ssl, contents, fileContents.size())) <= 0)
 	{
@@ -354,12 +371,11 @@ int main(int argc, char* argv[])
 			finish(ret);
 		}
 	}
-
-	cout << "    " << ret << " content bytes sent" << endl << endl;
+	cout << "  + " << ret << " content bytes sent" << endl;
 	cout.flush();
 
-	// Read response
-	cout << "  Reading from server...";
+	// Read the response
+	cout << "  . Reading from server...";
 	cout.flush();
 	while (true)
 	{
@@ -391,9 +407,15 @@ int main(int argc, char* argv[])
 
         len = ret;
         cout << len << " bytes read" << endl << endl << buf << endl;
+        cout.flush();
 	}
 
+	// Finish
+	cout << "  . Closing connection...";
+	cout.flush();
 	ssl_close_notify(&ssl);
+	cout << "done" << endl;
 
+	cleanUp();
 	exit(ret);
 }
